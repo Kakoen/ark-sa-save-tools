6dd584e5fc8601c36c7ffc3366647c7bea777b6e
kakoen <kakoen@gmail.com>
2023-11-28 10:26:10 +0100

    Improve JsonUtils, add convenience to output to String, reuse object writer.

--- src/main/java/net/kakoen/arksa/savetools/utils/JsonUtils.java
+++ src/main/java/net/kakoen/arksa/savetools/utils/JsonUtils.java
@@ -11,17 +11,31 @@ import java.nio.file.Path;
 
 public class JsonUtils {
 
-    public static void writeJsonToFile(Object object, Path file) throws IOException {
-        ObjectWriter writer = new ObjectMapper()
-                .setSerializationInclusion(JsonInclude.Include.NON_DEFAULT)
-                .writer(new DefaultPrettyPrinter());
+    private final static ObjectWriter DEFAULT_OBJECT_WRITER = new ObjectMapper()
+            .setSerializationInclusion(JsonInclude.Include.NON_DEFAULT)
+            .writer(new DefaultPrettyPrinter());
 
-        writeJsonToFile(object, file, writer);
+    public static void writeJsonToFile(Object object, Path file) throws IOException {
+        writeJsonToFile(object, file, DEFAULT_OBJECT_WRITER);
     }
 
     public static void writeJsonToFile(Object object, Path file, ObjectWriter objectWriter) throws IOException {
         Files.createDirectories(file.getParent());
         objectWriter.writeValue(file.toFile(), object);
     }
+
+    public static String toJsonString(Object object) {
+        return toJsonString(object, DEFAULT_OBJECT_WRITER);
+    }
+
+    public static String toJsonString(Object object, ObjectWriter objectWriter) {
+        try {
+            return objectWriter.writeValueAsString(object);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 
 }
