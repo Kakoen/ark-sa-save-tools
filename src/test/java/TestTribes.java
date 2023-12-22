96a5a1ba57992303c6ab360b5b10b55b3b168120
kakoen <kakoen@gmail.com>
2023-12-22 17:52:30 +0100

    Added test classes for tribes / profiles

---
+++ src/test/java/TestTribes.java
@@ -0,0 +1,30 @@
+import net.kakoen.arksa.savetools.ArkTribe;
+import net.kakoen.arksa.savetools.utils.JsonUtils;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class TestTribes {
+
+    Path testPath = Path.of("c:\\tmp\\save\\");
+
+    @Test
+    public void test() throws IOException {
+        // Try to read every .arktribe file by using ArkTribe constructor
+        // and print the result
+        Files.list(testPath)
+                .filter(path -> path.toString().endsWith(".arktribe"))
+                .forEach(path -> {
+                    try {
+                        ArkTribe tribe = new ArkTribe(path);
+                        JsonUtils.writeJsonToFile(tribe, Path.of(path.toString().replace(".arktribe", ".json")));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+}
